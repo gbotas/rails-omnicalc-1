f0ef37bca81d90fe0301a18954d738c255c7043f
gbotas <gbotas@uchicago.edu>
2025-04-22 21:04:51 +0000

    secondcommit

--- app/controllers/square_controller.rb
+++ app/controllers/square_controller.rb
@@ -1,9 +1,8 @@
 class SquareController < ApplicationController
 
   def square_input
-  end 
 
-  render( { : template => "input_templates/square"})
-end 
+  render( { :template => "/input_templates/square"})
+  end 
 end 
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
 get("/", { :controller => "square", :action => "square_input" })
-end
+
 
 get("/square/new", { :controller => "square", :action => "square_input"})
+
 end 
